46421cf3a02af99c07cfd0c1784db7661fcd3f48
Eli Richey <eli@elirichey.com>
2024-03-30 15:47:23 -0500

    cleanup;

--- utils/video.ts
+++ utils/video.ts
@@ -25,14 +25,12 @@ async function standardizeVideo(
         "-lavfi",
         "[0:v]scale=ih*16/9:-1,boxblur=luma_radius=min(h\\,w)/20:luma_power=1:chroma_radius=min(cw\\,ch)/20:chroma_power=1[bg];[bg][0:v]overlay=(W-w)/2:(H-h)/2,crop=h=iw*9/16",
       ])
-      //.output("output.mp4")
       .on("progress", ({ percent }) => utils.logProgress(percent / 2)) // divide by 2 because there's 2 videos, idk
       .on("end", () => resolve(tmpFilePath))
       .on("error", (err) => {
         utils.logError(err);
         resolve("");
       })
-      //.run();
       .save(tmpFilePath);
   });
 
